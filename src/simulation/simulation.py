3351b08ccc5276455bc2e621e2d4b3fffc387cf3
owen-sullivan <opsullivan85@gmail.com>
2025-09-04 16:43:48 -0400

    added back in controller

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -24,8 +24,15 @@ import numpy as np
 import torch
 from isaaclab.utils import configclass  # type: ignore
 from isaaclab.envs import ManagerBasedRLEnv, ManagerBasedRLEnvCfg  # type: ignore
+from isaaclab.scene import InteractiveSceneCfg
 
-from src.simulation.cfg.manager_components import ActionsCfg, EventsCfg, ObservationsCfg, RewardsCfg, TerminationsCfg
+from src.simulation.cfg.manager_components import (
+    ActionsCfg,
+    EventsCfg,
+    ObservationsCfg,
+    RewardsCfg,
+    TerminationsCfg,
+)
 from src.simulation.cfg.scene import SceneCfg
 from src.sim2real import SimInterface, VectObjectPool
 from src.simulation.util import (
@@ -71,6 +78,75 @@ class QuadrupedEnvCfg(ManagerBasedRLEnvCfg):
             scanner.update_period = self.decimation * self.sim.dt  # 50 Hz
 
 
+def controls_to_joint_efforts(
+    controls: np.ndarray, controllers: VectObjectPool, scene: InteractiveSceneCfg
+) -> torch.Tensor:
+    joint_pos = scene["robot"].data.joint_pos.cpu().numpy()
+    joint_vel = scene["robot"].data.joint_vel.cpu().numpy()
+    joint_states = isaac_joints_to_interface(joint_pos, joint_vel)
+
+    body_state = scene["robot"].data.root_state_w.cpu().numpy()
+    body_state = isaac_body_to_interface(body_state)
+
+    torques_interface = controllers.call(
+        function=SimInterface.get_torques,
+        mask=None,
+        joint_states=joint_states,
+        body_state=body_state,
+        command=controls,
+    )
+    torques_isaac_np = interface_to_isaac_torques(torques_interface)
+    torques_isaac = torch.from_numpy(torques_isaac_np).to(scene.device)
+    return torques_isaac
+
+
+def walk_in_place(
+    count: int, step_state: bool, control_interface: VectObjectPool
+) -> bool:
+    # step in place every 200ms
+    if count % 40 == 0:
+        if step_state:
+            control_interface.call(
+                SimInterface.initiate_footstep,
+                mask=None,
+                leg=np.repeat(np.array([0]), args_cli.num_envs),
+                location_hip=np.repeat(
+                    np.asarray([0.1, 0.1])[None, :], args_cli.num_envs, axis=0
+                ),
+                duration=np.repeat(np.array([0.2]), args_cli.num_envs),
+            )
+            control_interface.call(
+                SimInterface.initiate_footstep,
+                mask=None,
+                leg=np.repeat(np.array([3]), args_cli.num_envs),
+                location_hip=np.repeat(
+                    np.asarray([-0.1, -0.1])[None, :], args_cli.num_envs, axis=0
+                ),
+                duration=np.repeat(np.array([0.2]), args_cli.num_envs),
+            )
+        else:
+            control_interface.call(
+                SimInterface.initiate_footstep,
+                mask=None,
+                leg=np.repeat(np.array([1]), args_cli.num_envs),
+                location_hip=np.repeat(
+                    np.asarray([0.1, -0.1])[None, :], args_cli.num_envs, axis=0
+                ),
+                duration=np.repeat(np.array([0.2]), args_cli.num_envs),
+            )
+            control_interface.call(
+                SimInterface.initiate_footstep,
+                mask=None,
+                leg=np.repeat(np.array([2]), args_cli.num_envs),
+                location_hip=np.repeat(
+                    np.asarray([-0.1, 0.1])[None, :], args_cli.num_envs, axis=0
+                ),
+                duration=np.repeat(np.array([0.2]), args_cli.num_envs),
+            )
+        return not step_state
+    return step_state
+
+
 def main():
     """Main function."""
     # create environment configuration
@@ -79,23 +155,32 @@ def main():
     env_cfg.sim.device = args_cli.device
     # setup RL environment
     env = ManagerBasedRLEnv(cfg=env_cfg)
+    make_controllers = lambda: VectObjectPool(
+        instances=args_cli.num_envs,
+        cls=SimInterface,
+        dt=env_cfg.sim.dt,
+        # dt=env_cfg.sim.dt * env_cfg.decimation,
+        debug_logging=False,
+    )
+    step_state = False
 
     # simulate physics
     count = 0
-    while simulation_app.is_running():
-        with torch.inference_mode():
-            # # reset
-            # if count % 300 == 0:
-            #     count = 0
-            #     env.reset()
-            #     print("-" * 80)
-            #     print("[INFO]: Resetting environment...")
-            # sample random actions
-            joint_efforts = torch.randn_like(env.action_manager.action)
-            # step the environment
-            obs, rew, terminated, truncated, info = env.step(joint_efforts)
-            # update counter
-            count += 1
+    with make_controllers() as controllers:
+        while simulation_app.is_running():
+            with torch.inference_mode():
+
+                step_state = walk_in_place(count, step_state, controllers)
+                command = np.zeros((args_cli.num_envs, 3), dtype=np.float32)
+                command[:, 0] = 0.3
+                command[:, 2] = 0.2
+                joint_efforts = controls_to_joint_efforts(command, controllers, env.scene)
+
+                # step the environment
+                obs, rew, terminated, truncated, info = env.step(joint_efforts)
+
+                # update counter
+                count += 1
 
     # close the environment
     env.close()
