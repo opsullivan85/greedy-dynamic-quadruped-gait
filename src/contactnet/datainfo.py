070af087024bda8b936aa0cf85ed6b56bdfbe05e
owen-sullivan <opsullivan85@gmail.com>
2025-09-21 18:37:09 -0400

    added foot placement map histogram

--- src/contactnet/datainfo.py
+++ src/contactnet/datainfo.py
@@ -6,6 +6,9 @@ from src import PROJECT_ROOT, logger
 from src.contactnet.debug import view_footstep_cost_map
 from src.contactnet.tree import StepNode
 import src.simulation.cfg.footstep_scanner_constants as fs
+from src.contactnet.util import get_dataset_paths
+import matplotlib.pyplot as plt
+from matplotlib import colors
 
 
 parser = argparse.ArgumentParser(description="Data information")
@@ -14,16 +17,17 @@ parser.add_argument(
     action="store_true",
     help="Generate costmaps for the dataset"
 )
+parser.add_argument(
+    "--placement-maps",
+    action="store_true",
+    help="Generate footstep placement maps for the dataset"
+)
 args, unused_args = parser.parse_known_args()
 
 def log_data_info():
     """Logs information about the data files in the data directory."""
-    # load most recent data from
-    data_dir = PROJECT_ROOT / "data" / "datasets"
-    data_files = sorted(data_dir.glob("*.pkl"))
     total_points = 0
-    if not data_files:
-        raise FileNotFoundError(f"No data files found in {data_dir}")
+    data_files = get_dataset_paths()
     for data_file in data_files:
         with open(data_file, "rb") as f:
             data = pickle.load(f)
@@ -39,11 +43,7 @@ def log_data_info():
 
 def generate_all_costmaps():
     """Generates costmaps for all data files in the data directory."""
-    data_dir = PROJECT_ROOT / "data" / "datasets"
-    data_files = sorted(data_dir.glob("*.pkl"))
-    if not data_files:
-        raise FileNotFoundError(f"No data files found in {data_dir}")
-    for data_file in data_files:
+    for data_file in get_dataset_paths():
         with open(data_file, "rb") as f:
             data = pickle.load(f)
         logger.info(f"Generating costmaps for data file: {data_file}")
@@ -64,7 +64,68 @@ def generate_all_costmaps():
                 save_figure=True,
             )
 
+def generate_footstep_placement_maps():
+    """Generates 2D histogram maps of footstep placements."""
+
+    # Single plot with all feet overlaid
+    fig, ax = plt.subplots(figsize=(10, 10))
+    colormaps = ['Reds', 'Greens', 'Blues', 'Purples']
+    foot_positions_all = [[], [], [], []]
+
+    for data_file in get_dataset_paths():
+        with open(data_file, "rb") as f:
+            data = pickle.load(f)
+        
+        for datum in data['training_data']:
+            datum: StepNode
+            foot_positions = datum.state.obs.foot_positions_b
+            for i in range(4):
+                foot_positions_all[i].append(foot_positions[i][::-1])
+    
+    # Calculate max_density across all histograms
+    max_density = 1
+    for foot_positions in foot_positions_all:
+        foot_positions = np.array(foot_positions)
+        x = foot_positions[:, 0]
+        y = foot_positions[:, 1]
+        H, _, _ = np.histogram2d(x, y, bins=25)
+        max_density = max(max_density, np.max(H))
+    
+    norm = colors.LogNorm(1, float(max_density))
+    
+    for foot_positions, cmap in zip(foot_positions_all, colormaps):
+        foot_positions = np.array(foot_positions)
+        x = foot_positions[:, 0]
+        y = foot_positions[:, 1]
+        ax.hist2d(x, y, bins=75, cmap=cmap, alpha=1.0, norm=norm)
+    
+    # Set limits to enclose all data
+    all_x = []
+    all_y = []
+    for foot_positions in foot_positions_all:
+        foot_positions = np.array(foot_positions)
+        all_x.extend(foot_positions[:, 0])
+        all_y.extend(foot_positions[:, 1])
+    ax.set_xlim(min(all_x), max(all_x))
+    ax.set_ylim(min(all_y), max(all_y))
+    
+    ax.set_aspect('equal', 'box')
+    ax.set_xlabel("Lateral Foot Offset (m)")
+    ax.set_ylabel("Longitudinal Foot Offset (m)")
+    # ax.set_title("Footstep Placement Heatmaps")
+
+    # Add colorbar for density estimation
+    mappable = plt.cm.ScalarMappable(cmap='Reds', norm=norm)
+    cbar = fig.colorbar(mappable, ax=ax)
+    cbar.set_label("Footstep Placement Density\n(Scale shared across feet)")
+
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
     log_data_info()
     if args.costmaps:
         generate_all_costmaps()
+    if args.placement_maps:
+        generate_footstep_placement_maps()
