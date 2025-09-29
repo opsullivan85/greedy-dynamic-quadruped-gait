de601bd2550aa08b4f4de5bc308cd4cee5d8b9df
owen-sullivan <opsullivan85@gmail.com>
2025-09-28 22:45:54 -0400

    added termination for robot foot slipping off platform

--- src/gaitnet/env_cfg/terminations.py
+++ src/gaitnet/env_cfg/terminations.py
@@ -1,8 +1,28 @@
-from isaaclab.envs import mdp
+import torch
+from isaaclab.assets import ArticulationData, RigidObject
+from isaaclab.envs import ManagerBasedRLEnv, mdp
 from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.utils import configclass
 import math
+
+
+def height_below_minimum(
+    env: ManagerBasedRLEnv, minimum_height: float, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot")
+) -> torch.Tensor:
+    """Terminate when the asset's root height is below the minimum height.
+
+    Note:
+        This is currently only supported for flat terrains, i.e. the minimum height is in the world frame.
+    """
+    # extract the used quantities (to enable type-hinting)
+    data: ArticulationData = env.scene[asset_cfg.name].data
+    # properly index the body positions for the specified body IDs
+    body_pos = data.body_link_pos_w[:, asset_cfg.body_ids]  # (num_envs, num_bodies, 3)
+    heights = body_pos[:, :, 2]  # (num_envs, num_bodies)
+    # if any body is below the minimum height, terminate
+    dones = torch.any(heights < minimum_height, dim=1)
+    return dones
 
 
 @configclass
@@ -22,5 +42,12 @@ class TerminationsCfg:
         params={
             "minimum_height": 0.15,
             "asset_cfg": SceneEntityCfg("robot", body_names="trunk"),
+        },
+    )
+    foot_below_ground = DoneTerm(
+        func=height_below_minimum,
+        params={
+            "minimum_height": 0.0,
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*_foot"),
         },
     )
